359846209826076624db0a9eca3ff24147143999
riverson98 <riversonvicente@gmail.com>
2022-07-21 13:49:11 -0300

    correção da marca dos carros

--- main.java
+++ main.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class main {
     public static void main(String[] args) {
 
-        var civic = new Honda("Civic","model.Honda","150HP","Preto");
-        var kiwid = new Citroen("Kiwid","model.Citroen","90HP","Branco");
-        var gol = new Volkswagen("Gol","model.Volkswagen","100HP","Vermelho");
+        var civic = new Honda("Civic","Honda","150HP","Preto");
+        var kiwid = new Citroen("Kiwid","Citroen","90HP","Branco");
+        var gol = new Volkswagen("Gol","Volkswagen","100HP","Vermelho");
 
         var carros = List.of(civic,kiwid,gol);
 
